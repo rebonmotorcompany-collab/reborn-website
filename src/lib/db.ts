b7aad78264cd986e6e521dbc5bbbeadcc9c5f563
MoazamMuzamil707 <moazammuzamil707@gmail.com>
2026-07-16 14:38:41 +0500

    fix issue

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'mysql2/promise'
+import { PrismaMysql } from '@prisma/adapter-mysql'
+import mysql from 'mysql2/promise'
 
 const prismaClientSingleton = () => {
+  const connectionString = process.env.DATABASE_URL
+  const pool = mysql.createPool(connectionString as string)
+  const adapter = new PrismaMysql(pool)
+
   return new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
+    adapter,
   })
 }
 
